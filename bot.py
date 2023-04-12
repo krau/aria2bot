be18f910ee5debae0e2a34b4a63be5b59b5b789f
krau <krau39@outlook.com>
2023-04-12 10:47:32 +0800

    format code

--- bot.py
+++ bot.py
@@ -1,16 +1,42 @@
 import os
 
 from telegram import Update
-from telegram.ext import ContextTypes, ApplicationBuilder, CommandHandler, CallbackContext
+from telegram.ext import (
+    ApplicationBuilder,
+    CallbackContext,
+    CommandHandler,
+    ContextTypes,
+)
 
 from 源码.小工具 import 仅主人装饰器, 开始标记
+from 源码.技能.下载 import (
+    强制添加下载任务处理器,
+    添加下载任务处理器,
+)
+from 源码.技能.停止 import (
+    恢复所有任务处理器,
+    暂停所有任务处理器,
+    确认清空任务处理器,
+    请求清空任务处理器,
+)
+from 源码.技能.单独操作 import (
+    删除单任务处理器,
+    刷新单任务处理器,
+    操作单任务对话处理器,
+    暂停单任务处理器,
+    继续单任务处理器,
+)
+from 源码.技能.查询 import (
+    刷新下载器状态处理器,
+    刷新活跃任务处理器,
+    刷新等待中任务处理器,
+    回主菜单处理器,
+    查询下载器状态处理器,
+    查询活跃任务处理器,
+    查询等待中任务处理器,
+)
 from 源码.日志 import 日志器
 from 源码.配置 import 配置
-from 源码.技能.下载 import 添加下载任务处理器, 强制添加下载任务处理器
-from 源码.技能.查询 import 查询活跃任务处理器, 刷新活跃任务处理器, 回主菜单处理器, 查询下载器状态处理器
-from 源码.技能.查询 import 查询等待中任务处理器, 刷新等待中任务处理器, 刷新下载器状态处理器
-from 源码.技能.停止 import 恢复所有任务处理器, 暂停所有任务处理器, 确认清空任务处理器, 请求清空任务处理器
-from 源码.技能.单独操作 import 操作单任务对话处理器, 暂停单任务处理器, 继续单任务处理器, 删除单任务处理器, 刷新单任务处理器
 
 if not os.path.exists('./log'):
     os.mkdir('./log')
@@ -25,7 +51,8 @@ if 配置.代理地址:
 @仅主人装饰器
 async def 开始(更新: Update, 上下文: ContextTypes.DEFAULT_TYPE):
     日志器.info(f'{更新.effective_user.name} 执行了/start')
-    await 上下文.bot.send_message(chat_id=更新.effective_chat.id, text='欢迎使用Aria2Bot', reply_markup=开始标记, reply_to_message_id=更新.effective_message.id)
+    await 上下文.bot.send_message(chat_id=更新.effective_chat.id, text='欢迎使用Aria2Bot',
+                        reply_markup=开始标记, reply_to_message_id=更新.effective_message.id)
 
 
 async def 帮助(更新: Update, 上下文: ContextTypes.DEFAULT_TYPE):
