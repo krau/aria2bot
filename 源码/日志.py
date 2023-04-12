6d03e44e1d261bbba31ee91d7ee4f536251f1744
krau <krau39@outlook.com>
2023-04-12 10:50:32 +0800

    format

--- 源码/日志.py
+++ 源码/日志.py
@@ -2,10 +2,9 @@ import logging
 from logging import handlers
 from .配置 import 配置
 
-日志器 = logging.getLogger(name='ARIA2BOT')
+日志器 = logging.getLogger(name="ARIA2BOT")
 日志格式 = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s: - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+    "%(asctime)s - %(name)s - %(levelname)s: - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
 )
 流处理器 = logging.StreamHandler()
 流处理器.setLevel(配置.控制台日志等级)
@@ -13,11 +12,7 @@ from .配置 import 配置
 日志器.addHandler(流处理器)
 
 时旋文件处理器 = handlers.TimedRotatingFileHandler(
-    filename='./log/aria2bot.log',
-    when='D',
-    interval=1,
-    backupCount=7,
-    encoding='utf-8'
+    filename="./log/aria2bot.log", when="D", interval=1, backupCount=7, encoding="utf-8"
 )
 
 时旋文件处理器.setLevel(配置.文件日志等级)
